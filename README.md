8ef5066721c33a52a7d415284919d711208003ee
jimb0 <jimccann@redhat.com>
2024-09-04 15:27:06 -0400

    nested dhcp

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nested-ovf-ansible
 Create a dynamic vsphere setup (using dhcp)
+
+You will need a server to host your ovf files via http. I suggest: https://github.com/svenstaro/miniserve or you could use a s3 bucket.
+
+I got my ESXi ova from Willaim Lam (just google)
+I got my vCenter ova from the vCenter install ISO (download from vmware)
+
+look at file runitall.sh for examples on how to run
---
+++ runitall.sh
@@ -0,0 +1,13 @@
+
+#ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname-host.vpshere.local,nested8-myjobname-host1.vsphere.local]}'
+#ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var testingesxi=true --extra-var testingvc=true
+#ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var testingesxi=false --extra-var testingvc=false
+
+
+
+ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' 
+
+echo "sleep for 1 hour"
+sleep 3600
+
+ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var removevsphere=true -t removevsphere
