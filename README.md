1a78d1dd42bb598039a7d6e12d753919a11fc416
jimb0 <jimccann@redhat.com>
2024-09-04 23:27:58 -0400

    nested dhcp

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Create a dynamic vsphere setup (using dhcp)
 You will need a server to host your ova files via http. I suggest miniserver (https://github.com/svenstaro/miniserve) or you could use some type of s3 bucket. You will also need a dhcp server on your network that you create your nested vSphere in.
 
 I got my ESXi ova from William Lam (https://williamlam.com/)
-I got my vCenter ova from the vCenter install ISO (download from vmware)
+I got my vCenter ova from the vCenter install ISO (download from VMware/Broadcom)
 
 look at file runitall.sh for examples on how to run
 
--- runitall.sh
+++ runitall.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 #ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname-host.vpshere.local,nested8-myjobname-host1.vsphere.local]}'
 #ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var testingesxi=true --extra-var testingvc=true
 #ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var testingesxi=false --extra-var testingvc=false
