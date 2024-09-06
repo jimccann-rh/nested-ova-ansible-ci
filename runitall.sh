22325165f46c99555b4041a5d865014561a4af14
jimb0 <jimccann@redhat.com>
2024-09-06 00:39:27 -0400

    nested dhcp

--- runitall.sh
+++ runitall.sh
@@ -6,28 +6,33 @@
 
 echo "https://github.com/jimccann-rh/nested-ova-ansible"
 
-echo "going to delete nested in 5 seconds"
+echo "@@@ GOING TO DELETE NESTED VM'S IN 5 SECONDS"
 sleep 10
 
 echo "ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var removevsphere=true -t removevsphere"
 echo 'ansible-playbook -i hosts main.yml --extra-var version="7" --extra-var='{"target_hosts": [nested7-myjobname1-host.vpshere.local,nested7-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested7-myjobname-VC.vpshere.local]}' --extra-var removevsphere=true -t removevsphere'
 
+echo "@@@"
 time ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var removevsphere=true -t removevsphere
-
+echo "@@@"
 time ansible-playbook -i hosts main.yml --extra-var version="7" --extra-var='{"target_hosts": [nested7-myjobname1-host.vpshere.local,nested7-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested7-myjobname-VC.vpshere.local]}' --extra-var removevsphere=true -t removevsphere
 
-echo "going to create vsphere environments"
+echo "@@@ GOING TO CREATE NESTED VSPHERE ENVIRONMENT"
 echo 'ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var esximemory="65536" --extra-var esxicpu="16" '
 echo 'ansible-playbook -i hosts main.yml --extra-var version="7" --extra-var='{"target_hosts": [nested7-myjobname1-host.vpshere.local]}' --extra-var='{"target_vcs": [nested7-myjobname-VC.vpshere.local]}' --extra-var esximemory="65536" --extra-var esxicpu="16"'
 
+echo "@@@"
 time ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var esximemory="65536" --extra-var esxicpu="16"
 
+echo "@@@"
 time ansible-playbook -i hosts main.yml --extra-var version="7" --extra-var='{"target_hosts": [nested7-myjobname1-host.vpshere.local]}' --extra-var='{"target_vcs": [nested7-myjobname-VC.vpshere.local]}' --extra-var esximemory="65536" --extra-var esxicpu="16"
 
-echo "rerun script for testing"
+echo "@@@ RERUN SCRIPT FOR CONFIRMING ENVIRONMENT"
 
+echo "@@@"
 time ansible-playbook -i hosts main.yml --extra-var version="8" --extra-var='{"target_hosts": [nested8-myjobname1-host.vpshere.local,nested8-myjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested8-myjobname-VC.vpshere.local]}' --extra-var esximemory="65536" --extra-var esxicpu="16"
 
+echo "@@@"
 time ansible-playbook -i hosts main.yml --extra-var version="7" --extra-var='{"target_hosts": [nested7-myjobname1-host.vpshere.local]}' --extra-var='{"target_vcs": [nested7-myjobname-VC.vpshere.local]}' --extra-var esximemory="65536" --extra-var esxicpu="16"
 
-echo "done"
+echo "@@@ DONE"
