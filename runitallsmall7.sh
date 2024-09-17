c808d87b68f1d90f67fadfcbd1b9ed081a67d674
jimb0 <jimccann@redhat.com>
2024-09-17 11:57:16 -0400

    clean up

--- runitallsmall7.sh
+++ runitallsmall7.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "@@@ GOING TO CREATE NESTED VSPHERE ENVIRONMENT"
-ansible-playbook -i hosts main.yml --extra-var version="7" --extra-var='{"target_hosts": [nested7-singlemyjobname1-host.vpshere.local]}' --extra-var='{"target_vcs": [nested7-singlemyjobname-VC.vpshere.local]}' --extra-var esximemory="65536" --extra-var esxicpu="16"
+ansible-playbook -i hosts main.yml --extra-var version="7" --extra-var='{"target_hosts": [nested7-singlemyjobname1-host.vpshere.local,nested7-singlemyjobname2-host.vpshere.local]}' --extra-var='{"target_vcs": [nested7-singlemyjobname-VC.vpshere.local]}' --extra-var esximemory="65536" --extra-var esxicpu="16"
 
 
 echo "@@@ DONE"
